957635abda33f18cae8f7ef64816af8dbb084a25
VAIBHAV <pathakvaibhav755@gmail.comgit config --global user.name VAIBHAV>
2025-12-07 14:09:12 +0530

    added new file

---
+++ next.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // Disable powered by header for security
+  poweredByHeader: false,
+  
+  // Enable React strict mode
+  reactStrictMode: true,
+};
+
+module.exports = nextConfig;
